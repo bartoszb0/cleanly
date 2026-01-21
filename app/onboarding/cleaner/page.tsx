360cf14895dfd2281672feec4c0b47526114dde4
bartoszb0 <bartoszb2020@gmail.com>
2026-01-21 17:09:26 +0100

    feat: enhance onboarding UI with gradient backgrounds and remove layout component from onboarding descendants

--- app/onboarding/cleaner/page.tsx
+++ app/onboarding/cleaner/page.tsx
@@ -1,3 +1,7 @@
 export default function CleanerForm() {
-  return <div>CleanerForm</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-800 via-slate-800 to-green-900 flex items-center justify-center p-4">
+      CleanerForm
+    </div>
+  );
 }
--- app/onboarding/customer/page.tsx
+++ app/onboarding/customer/page.tsx
@@ -1,3 +1,7 @@
 export default function CustomerForm() {
-  return <div>CustomerForm</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br  from-sky-900 via-slate-800 to-slate-800 flex items-center justify-center p-4">
+      CustomerForm
+    </div>
+  );
 }
