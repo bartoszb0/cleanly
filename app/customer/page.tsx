7e45bf17df29eb336a56db6e4430c79a89da5479
bartoszb0 <bartoszb2020@gmail.com>
2026-01-28 20:02:36 +0100

    added first version of customer homepage

--- app/customer/page.tsx
+++ app/customer/page.tsx
@@ -1,9 +1,33 @@
-import { LogoutButton } from "@/components/logout-button";
+import CleanersList from "@/components/customer/cleaner-list";
+import CleanerListSkeleton from "@/components/customer/cleaner-list-skeleton";
+import { Button } from "@/components/ui/button";
+import { getCurrentCustomer } from "@/lib/queries/user";
+import getUppercaseCityName from "@/lib/utils/getUppercaseCity";
+import { Suspense } from "react";
 
-export default function CustomerPage() {
+export default async function CustomerPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) {
+  const { page } = await searchParams;
+  const currentPage = Number(page) || 1;
+  const user = await getCurrentCustomer();
+
   return (
-    <div>
-      CustomerPage <LogoutButton />
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold mt-10 text-center">
+        Cleaners in {getUppercaseCityName(user.city)}
+      </h1>
+      <div className="flex flex-row gap-4 mt-5">
+        {/* TODO */}
+        <Button>Input - Search by name</Button>
+        <Button>Sort</Button>
+        <Button>Filter</Button>
+      </div>
+      <Suspense fallback={<CleanerListSkeleton />}>
+        <CleanersList user={user} page={currentPage} />
+      </Suspense>
     </div>
   );
 }
