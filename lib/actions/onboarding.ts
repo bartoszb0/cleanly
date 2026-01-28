a0fd2eb7ad387b3936e95285191054f9fad4ad71
bartoszb0 <bartoszb2020@gmail.com>
2026-01-28 20:55:56 +0100

    removed redundant try catch blocks and fixed error naming in finishCleanerOnboarding

--- lib/actions/onboarding.ts
+++ lib/actions/onboarding.ts
@@ -3,7 +3,6 @@
 import { CleanerFormValues, CleanerSchema } from "@/lib/schemas/cleanerForm";
 import { CustomerFormValues, CustomerSchema } from "@/lib/schemas/customerForm";
 import { createClient } from "@/lib/supabase/server";
-import { getErrorMessage } from "@/lib/utils/getErrorMessage";
 import { getRequiredUser } from "../data/auth";
 
 export async function assignRole(selectedRole: UserProfileRole) {
@@ -39,36 +38,32 @@ export async function finishCustomerOnboarding(userData: CustomerFormValues) {
   const user = await getRequiredUser();
   const supabase = await createClient();
 
-  try {
-    const { error: customerError } = await supabase.from("customers").insert({
-      id: user.id,
-      full_name: data.fullName,
-      city: data.city,
-      address: data.address,
-      post_code: data.postCode,
-      phone: data.phone,
-      has_pets: data.hasPets,
-    });
+  const { error: customerError } = await supabase.from("customers").insert({
+    id: user.id,
+    full_name: data.fullName,
+    city: data.city,
+    address: data.address,
+    post_code: data.postCode,
+    phone: data.phone,
+    has_pets: data.hasPets,
+  });
 
-    if (customerError) {
-      return { success: false, error: customerError.message };
-    }
-
-    const { error: onboardingError } = await supabase
-      .from("profiles")
-      .update({ onboarded: true })
-      .eq("id", user.id);
-
-    // Manual rollback in case user gets onboarding error
-    if (onboardingError) {
-      await supabase.from("customers").delete().eq("id", user.id);
-      return { success: false, error: onboardingError.message };
-    }
-
-    return { success: true };
-  } catch (error) {
-    return { success: false, error: getErrorMessage(error) };
+  if (customerError) {
+    return { success: false, error: customerError.message };
   }
+
+  const { error: onboardingError } = await supabase
+    .from("profiles")
+    .update({ onboarded: true })
+    .eq("id", user.id);
+
+  // Manual rollback in case user gets onboarding error
+  if (onboardingError) {
+    await supabase.from("customers").delete().eq("id", user.id);
+    return { success: false, error: onboardingError.message };
+  }
+
+  return { success: true };
 }
 
 export async function finishCleanerOnboarding(userData: CleanerFormValues) {
@@ -85,35 +80,31 @@ export async function finishCleanerOnboarding(userData: CleanerFormValues) {
   const user = await getRequiredUser();
   const supabase = await createClient();
 
-  try {
-    const { error: customerError } = await supabase.from("cleaners").insert({
-      id: user.id,
-      name: data.name,
-      bio: data.bio,
-      experience_years: data.experience,
-      hourly_rate: data.hourly_rate,
-      city: data.city,
-      phone: data.phone,
-      supplies_provided: data.supplies_provided,
-    });
+  const { error: cleanerError } = await supabase.from("cleaners").insert({
+    id: user.id,
+    name: data.name,
+    bio: data.bio,
+    experience_years: data.experience,
+    hourly_rate: data.hourly_rate,
+    city: data.city,
+    phone: data.phone,
+    supplies_provided: data.supplies_provided,
+  });
 
-    if (customerError) {
-      return { success: false, error: customerError.message };
-    }
-
-    const { error: onboardingError } = await supabase
-      .from("profiles")
-      .update({ onboarded: true })
-      .eq("id", user.id);
-
-    // Manual rollback in case user gets onboarding error
-    if (onboardingError) {
-      await supabase.from("cleaners").delete().eq("id", user.id);
-      return { success: false, error: onboardingError.message };
-    }
-
-    return { success: true };
-  } catch (error) {
-    return { success: false, error: getErrorMessage(error) };
+  if (cleanerError) {
+    return { success: false, error: cleanerError.message };
   }
+
+  const { error: onboardingError } = await supabase
+    .from("profiles")
+    .update({ onboarded: true })
+    .eq("id", user.id);
+
+  // Manual rollback in case user gets onboarding error
+  if (onboardingError) {
+    await supabase.from("cleaners").delete().eq("id", user.id);
+    return { success: false, error: onboardingError.message };
+  }
+
+  return { success: true };
 }
