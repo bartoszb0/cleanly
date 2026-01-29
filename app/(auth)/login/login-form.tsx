6ce788b35e3d7e95198e5c8a4d1acea5eb8bbe44
bartoszb0 <bartoszb2020@gmail.com>
2026-01-29 17:12:05 +0100

    updated utility function import

--- app/(auth)/login/login-form.tsx
+++ app/(auth)/login/login-form.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { createClient } from "@/lib/supabase/client";
-import { cn } from "@/lib/utils/cn";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
