450be74e11b23a95112b079b5c80e051be783742
bartoszb0 <bartoszb2020@gmail.com>
2026-01-30 13:30:37 +0100

    add cleaner opinion to cleaner page

--- app/customer/cleaner/[id]/page.tsx
+++ app/customer/cleaner/[id]/page.tsx
@@ -1,15 +1,19 @@
-import CleanerCalendar from "@/components/cleaner/profile/cleaner-calendar";
-import CleanerCalendarSkeleton from "@/components/cleaner/profile/cleaner-calendar-skeleton";
 import CleanerInfo from "@/components/cleaner/profile/cleaner-info";
 import CleanerInfoSkeleton from "@/components/cleaner/profile/cleaner-info-skeleton";
+import CleanerOpinions from "@/components/cleaner/profile/opinions/cleaner-opinions";
+import CleanerOpinionsSkeleton from "@/components/cleaner/profile/opinions/cleaner-opinions-skeleton";
 import { Suspense } from "react";
 
 export default async function CleanerPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ page: string }>;
 }) {
   const { id } = await params;
+  const { page } = await searchParams;
+  const currentPage = Number(page) || 1;
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 py-12 px-4">
@@ -17,8 +21,8 @@ export default async function CleanerPage({
         <Suspense fallback={<CleanerInfoSkeleton />}>
           <CleanerInfo id={id} />
         </Suspense>
-        <Suspense fallback={<CleanerCalendarSkeleton />}>
-          <CleanerCalendar id={id} />
+        <Suspense fallback={<CleanerOpinionsSkeleton />}>
+          <CleanerOpinions id={id} page={currentPage} />
         </Suspense>
       </div>
     </div>
