d6336ad6e09dfccfaab826074bb47e2738badd94
bartoszb0 <bartoszb2020@gmail.com>
2026-02-05 11:52:33 +0100

    added filter controls expandable div

--- components/cleaner/filter-controls/index.tsx
+++ components/cleaner/filter-controls/index.tsx
@@ -1,14 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SlidersHorizontal } from "lucide-react";
+import { useState } from "react";
 import FilterCleanersDialog from "./filter-dialog";
 import SearchCleanerInput from "./search-input";
 import SortCleanersSelect from "./sort-select";
 
 export default function FilterControls() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="flex flex-col items-center gap-3 mt-5">
-      <SearchCleanerInput />
-      <div className="flex flex-row gap-2">
-        <SortCleanersSelect />
-        <FilterCleanersDialog />
+    <div className="mt-4">
+      <div className="flex flex-row items-center gap-2">
+        <SearchCleanerInput />
+        <Button
+          className={`h-12 transition-colors ${open ? "bg-sky-700" : "bg-sky-800 hover:bg-sky-600"}`}
+          onClick={() => setOpen(!open)}
+        >
+          <SlidersHorizontal />
+        </Button>
+      </div>
+
+      <div
+        className={`
+      overflow-hidden transition-all duration-300 ease-in-out
+      ${open ? "max-h-24 opacity-100 mt-4" : "max-h-0 opacity-0 mt-0"}
+    `}
+      >
+        <div className="flex flex-row items-center justify-center gap-2">
+          <SortCleanersSelect />
+          <FilterCleanersDialog />
+        </div>
       </div>
     </div>
   );
