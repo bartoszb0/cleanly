7777814a09439efa7a8bb544699f3ebbda092baa
bartoszb0 <bartoszb2020@gmail.com>
2026-02-11 23:39:46 +0100

    chanded css styling

--- app/customer/cleaner/[id]/page.tsx
+++ app/customer/cleaner/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function CleanerPage({
   const { id } = await params;
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 py-12 px-4">
+    <div className="min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 py-12 px-4">
       <div className="max-w-4xl mx-auto">
         <Suspense fallback={<CleanerInfoSkeleton />}>
           <CleanerInfo id={id} />
