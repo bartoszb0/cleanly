2f61655092c7b7f0cc13f7a353861e5c844f827e
bartoszb0 <bartoszb2020@gmail.com>
2026-02-12 17:57:54 +0100

    added constants for booking

---
+++ lib/constants/booking.ts
@@ -0,0 +1,5 @@
+export const VALID_HOURS = Array.from({ length: 14 }, (_, i) =>
+  (i + 7).toString(),
+);
+export const VALID_MINUTES = ["00", "15", "30", "45"];
+export const VALID_DURATIONS = ["1", "2", "3", "4", "5", "6"];
