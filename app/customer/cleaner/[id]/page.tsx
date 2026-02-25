cfa91c874791918eaba50df966da1f3aee178831
bartoszb0 <bartoszb2020@gmail.com>
2026-02-25 18:50:32 +0100

    moved UUID validation check into cleaner page out of query call

--- app/customer/cleaner/[id]/page.tsx
+++ app/customer/cleaner/[id]/page.tsx
@@ -2,7 +2,10 @@ import CleanerInfo from "@/components/cleaner/profile/cleaner-info";
 import CleanerInfoSkeleton from "@/components/cleaner/profile/cleaner-info-skeleton";
 import CleanerOpinions from "@/components/cleaner/profile/opinions/cleaner-opinions";
 import CleanerOpinionsSkeleton from "@/components/cleaner/profile/opinions/cleaner-opinions-skeleton";
+import { getCleaner } from "@/lib/data/cleaners";
+import { notFound } from "next/navigation";
 import { Suspense } from "react";
+import z from "zod";
 
 export default async function CleanerPage({
   params,
@@ -11,14 +14,20 @@ export default async function CleanerPage({
 }) {
   const { id } = await params;
 
+  if (!z.uuid().safeParse(id).success) return notFound();
+
+  const cleaner = await getCleaner(id);
+
+  if (!cleaner) return notFound();
+
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 py-12 px-4">
       <div className="max-w-4xl mx-auto">
         <Suspense fallback={<CleanerInfoSkeleton />}>
-          <CleanerInfo id={id} />
+          <CleanerInfo cleaner={cleaner} />
         </Suspense>
         <Suspense fallback={<CleanerOpinionsSkeleton />}>
-          <CleanerOpinions id={id} />
+          <CleanerOpinions cleaner={cleaner} />
         </Suspense>
       </div>
     </div>
