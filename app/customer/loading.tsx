d79e98404e7db8e6308ab65b941ab5528c89574e
bartoszb0 <bartoszb2020@gmail.com>
2026-02-25 18:49:52 +0100

    updated loading for customer homepage

--- app/customer/loading.tsx
+++ app/customer/loading.tsx
@@ -3,19 +3,15 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center w-full">
-      {/* Title Skeleton */}
-      <Skeleton className="h-10 w-72 mt-10" />
-
-      {/* FilterControls Skeleton */}
-      <div className="mt-4 flex flex-row items-center gap-2">
-        {/* Match the Search Input width/height */}
-        <Skeleton className="h-12 w-[300px] md:w-[400px]" />
-        {/* Match the Filter Toggle Button */}
-        <Skeleton className="h-12 w-12" />
+    <div className="flex flex-col items-center">
+      <div className="p-4">
+        <Skeleton className="h-10 w-72 mt-10 mx-auto" />
+        <div className="mt-4 flex flex-row items-center gap-2">
+          <Skeleton className="h-12 w-full" />
+          <Skeleton className="h-12 w-12" />
+          <Skeleton className="h-12 w-12" />
+        </div>
       </div>
-
-      {/* List Content */}
       <CleanersListSkeleton />
     </div>
   );
