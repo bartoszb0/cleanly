c8b6473f225d4c4628eebc7794e14db69eea646e
bartoszb0 <bartoszb2020@gmail.com>
2026-03-04 18:02:14 +0100

    refactor CustomerLayout component for improved structure and styling

--- app/customer/layout.tsx
+++ app/customer/layout.tsx
@@ -13,9 +13,11 @@ export default function CustomerLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen">
+    <div className="flex flex-col min-h-dvh">
       <Navbar />
-      <main className="pt-16 md:pt-0 md:ml-64">{children}</main>
+      <main className="flex flex-col flex-1 pt-16 md:pt-0 md:ml-64">
+        {children}
+      </main>
     </div>
   );
 }
