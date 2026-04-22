5673ae5f458b568a120b7599a289214d76a65b04
bartoszb0 <bartoszb2020@gmail.com>
2026-04-22 13:57:48 +0200

    fix page main styling

--- app/customer/messages/page.tsx
+++ app/customer/messages/page.tsx
@@ -1,6 +1,6 @@
 export default async function MessagesPage() {
   return (
-    <div className="flex flex-col gap-2 text-center items-center justify-center h-full">
+    <div className="flex flex-col gap-2 text-center bg-muted/50 items-center justify-center h-full">
       <h1 className="text-zinc-600">Open chat to start a conversation</h1>
     </div>
   );
